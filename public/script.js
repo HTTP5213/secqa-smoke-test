f1e8c5f643c209ae7ddf4fef89ee93611e97df13
simon.borer <simon.borer@gmail.com>
2023-03-03 09:54:31 -0500

    fixed assert

--- public/script.js
+++
@@ -1,7 +0,0 @@
-// Change string values to satisfy the assertion
-
-document.body.style.background = 'rgb(138, 255, 212)';
-
-console.assert(document.body.style.background === 'rgb(138, 255, 211)', {
-    errorMsg: "Nope, it's " + document.body.style.background
-});
